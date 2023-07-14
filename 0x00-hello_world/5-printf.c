c4f2688634d9598f6c08609b9482d664da0642d3
Hotzpeez <nappypee@yahoo.com>
2023-07-14 22:00:06 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * return: 0 (success)
+ * Return: 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcomeis a place of art,\n");
