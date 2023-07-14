269c4a1cce5d8c320c2aaeaf41bed01b72cf5c75
Hotzpeez <nappypee@yahoo.com>
2023-07-14 21:54:14 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * return: a;ways 0 (success)
- * */
+ * return: 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcomeis a place of art,\n");
-return (0);
+	return (0);
 }
