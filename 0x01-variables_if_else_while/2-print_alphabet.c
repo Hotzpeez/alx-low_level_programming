af5bc085dd061037549576fa09a338856009b424
Hotzpeez <nappypee@yahoo.com>
2023-07-16 13:02:06 +0100

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 /**
- * main -prints the alphabet.
- *
+ * main - prints the alphabetic *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i
-		for (i = 0: i < 26; i++)
-		{
-			putchar(alph[i]):
-		}
-		putchar('\n'):
-			return (0):
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
 }
