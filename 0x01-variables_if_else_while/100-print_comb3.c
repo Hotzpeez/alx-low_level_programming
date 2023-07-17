181bf6e330f19b4712b192e4162d9ef36fe3f9f6
Hotzpeez <nappypee@yahoo.com>
2023-07-17 01:26:20 +0100

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,21 +11,17 @@ int main(void)
 
 {
 	int n, m;
-	
-	for (n = '0'; n < '9'; n++)
+
+for (n = '0'; n < '9'; n++)
+	{
+	for (m = n + 1; m <= '9'; m++)
+	}
+	if (n != m)
 	{
 
-	for (m = n+1; m <= '9'; m++)
-	{
-	
-	if (n != m)
-	
-	{
-	
 	putchar(n);
 	putchar(m);
 	if (n == '8' && m == '9')
-	
 	continue;
 	putchar(',');
 	putchar(' ');
@@ -33,6 +29,6 @@ int main(void)
 	}
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
